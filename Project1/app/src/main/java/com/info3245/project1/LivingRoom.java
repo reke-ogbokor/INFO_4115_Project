35ec4f34fa1f011a840ff4b2b6ec3567efaa0d10
Matthew Finlayson <matt@DESKTOP-4VF9R8R.localdomain>
2020-11-18 17:06:35 -0800

    small changes to main menu

---
+++ Project1/app/src/main/java/com/info3245/project1/LivingRoom.java
@@ -0,0 +1,14 @@
+package com.info3245.project1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LivingRoom extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_living_room);
+    }
+}
---
+++ Project1/app/src/main/java/com/info3245/project1/ReadingRoom.java
@@ -0,0 +1,14 @@
+package com.info3245.project1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ReadingRoom extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reading_room);
+    }
+}
