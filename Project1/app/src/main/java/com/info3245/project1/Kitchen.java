7bcf2f5ff353186de4e23bf62241239a7f8791fe
Matthew Finlayson <matt@DESKTOP-4VF9R8R.localdomain>
2020-11-22 19:41:43 -0800

    Aplha Done

--- Project1/app/src/main/java/com/info3245/project1/Kitchen.java
+++ Project1/app/src/main/java/com/info3245/project1/Kitchen.java
@@ -1,14 +1,43 @@
 package com.info3245.project1;
 
-import androidx.appcompat.app.AppCompatActivity;
+        import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+        import android.os.Bundle;
+        import android.view.View;
+        import android.widget.Button;
+        import android.widget.CompoundButton;
+        import android.widget.ImageView;
+        import android.widget.ToggleButton;
 
 public class Kitchen extends AppCompatActivity {
+
+    ImageView ddImage;
+    ToggleButton chgBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_kitchen);
+
+        chgBtn = findViewById(R.id.dayDarkBtn);
+
+        chgBtn.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    ddImage = findViewById(R.id.imageView);
+                    ddImage.setImageResource(R.drawable.kitchen);
+
+                } else {
+                    ddImage = findViewById(R.id.imageView);
+                    ddImage.setImageResource(R.drawable.kitchendark);
+
+                }
+            }
+        });
+
+
+
+
     }
 }
--- Project1/app/src/main/java/com/info3245/project1/MainActivity.java
+++ Project1/app/src/main/java/com/info3245/project1/MainActivity.java
@@ -2,14 +2,23 @@ package com.info3245.project1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.SeekBar;
 import android.widget.TextView;
+import java.util.Calendar;
+import java.util.Date;
 
 
 public class MainActivity extends AppCompatActivity {
 
     TextView tvProgressLabel;
+
+
 
 
     @Override
@@ -22,13 +31,54 @@ public class MainActivity extends AppCompatActivity {
         SeekBar seekBar = findViewById(R.id.seekBar);
         seekBar.setOnSeekBarChangeListener(seekBarChangeListener);
 
-        int progress = seekBar.getProgress();
+        final int progress = seekBar.getProgress();
         tvProgressLabel = findViewById(R.id.textView15);
         tvProgressLabel.setText("Temperature: " + progress);
 
 
+        ImageButton kitchenBtn = (ImageButton) findViewById(R.id.imageKitchen);
+        ImageButton readingBtn = (ImageButton) findViewById(R.id.imageRRRoom);
+        ImageButton livingBtn = (ImageButton) findViewById(R.id.imageLRoom);
+        ImageButton outsideBtn = (ImageButton) findViewById(R.id.imageOutside);
+
+
+        kitchenBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, Kitchen.class));
+            }
+        });
+
+
+        readingBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, ReadingRoom.class));
+            }
+        });
+
+
+        livingBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, LivingRoom.class));
+            }
+        });
+
+
+        outsideBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, Outside.class));
+            }
+        });
+
+
 
     }
+
+
+
 
 
     SeekBar.OnSeekBarChangeListener seekBarChangeListener = new SeekBar.OnSeekBarChangeListener() {
@@ -37,6 +87,30 @@ public class MainActivity extends AppCompatActivity {
         public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
             // updated continuously as the user slides the thumb
             tvProgressLabel.setText("Temperature: " + progress);
+
+            progress = seekBar.getProgress();
+            tvProgressLabel.setText("Temperature: " + progress);
+
+            if (progress > 20){
+
+                ImageView img=(ImageView)findViewById(R.id.imageView4);
+                img.setBackgroundResource(R.drawable.hotgif);
+            }else if (progress > 15){
+
+                ImageView img=(ImageView)findViewById(R.id.imageView4);
+                img.setBackgroundResource(R.drawable.warmday);
+
+            }else if (progress > 5){
+
+                ImageView img=(ImageView)findViewById(R.id.imageView4);
+                img.setBackgroundResource(R.drawable.mediumcold);
+
+            }else {
+
+                ImageView img=(ImageView)findViewById(R.id.imageView4);
+                img.setBackgroundResource(R.drawable.cold2);
+
+            }
         }
 
         @Override
